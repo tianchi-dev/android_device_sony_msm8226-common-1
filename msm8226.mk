f2df0e954454c7c3a9f14bd8123911208b078829
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2014-08-13 14:46:18 +0530

    msm8226: Split line build error fix [TEST]

--- msm8226.mk
+++ msm8226.mk
@@ -190,18 +190,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Radio and Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libargs=-d /dev/smd0 \
-    persist.rild.nitz_plmn= \
-    persist.rild.nitz_long_ons_0= \
-    persist.rild.nitz_long_ons_1= \
-    persist.rild.nitz_long_ons_2= \
-    persist.rild.nitz_long_ons_3= \
-    persist.rild.nitz_short_ons_0= \
-    persist.rild.nitz_short_ons_1= \
-    persist.rild.nitz_short_ons_2= \
-    persist.rild.nitz_short_ons_3= \
     ril.subscription.types=NV,RUIM \
-    DEVICE_PROVISIONED=1 \
     persist.radio.msgtunnel.start=false
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
